8d9db9f4975c03879a80fd8162ff695df46b6d37
Mohsin Nabeel <mohsinnabeeldummy@Mohsins-MacBook-Air.local>
2019-04-06 23:00:35 +0500

    Assignment

---
+++ main.go
@@ -0,0 +1,92 @@
+
+
+package main
+
+
+
+
+import  "fmt" 
+import "math"
+//import "strings"
+
+
+
+
+func ConcatSlice(sliceToConcat []byte) string {
+
+	myString := "cancatinating "
+
+	for i := 0 ; i < len(sliceToConcat) ; i++ {
+
+	
+
+		myString += string(sliceToConcat[i])
+		myString += "-"
+	}
+
+	myString += string(sliceToConcat[len(sliceToConcat)-1])
+
+
+	return myString
+
+}
+
+
+func Encrypt(sliceToEncrypt []byte, ceaserCount int) {
+
+	myString := ""
+
+	for i := 0 ; i < len(sliceToEncrypt) ; i++ {
+
+	
+
+		myString += string(sliceToEncrypt[i])
+	}
+
+
+	tempString := ""
+
+	for i := 0 ; i < len(myString) ; i++ {
+
+		tempString += string(int(math.Mod(float64(int(myString[i]) - 97 + ceaserCount), 26)) +97)
+
+
+
+	}
+
+	for i := 0 ; i < len(sliceToEncrypt) ; i++ {
+
+	
+
+		sliceToEncrypt[i] += tempString[i]
+	}
+
+
+	fmt.Println("Encrypted String is :   " , sliceToEncrypt )
+	//fmt.Println(sliceToEncrypt)
+
+	//return tempString
+
+
+
+}
+
+
+
+
+
+
+func main(){
+
+	byteArray := []byte{'J', 'O', 'H', 'N'}
+
+	Encrypt(byteArray, 10)
+
+}
+
+
+
+
+
+
+
